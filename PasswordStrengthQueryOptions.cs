06122d31a62b98aff83e143133781ececfde5509
APIVerve Team <hello@apiverve.com>
2024-09-21 01:18:46 -0500

    Bug Fixes

---
+++ PasswordStrengthQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class PasswordStrengthQueryOptions {
+public string password { get; set; }
+}
+
+}
